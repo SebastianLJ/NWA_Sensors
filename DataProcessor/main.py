a4bf0a0572a9fb39523cd7554bd8e2b81c6e33d0
SebastianLJ <sebastian.lund.j@gmail.com>
2021-03-26 14:19:16 +0100

    fixed structure for data processor

---
+++ DataProcessor/main.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+import csv
+import re
+from datetime import datetime
+
+x = []
+rhum = []
+avghum = []
+window = []
+
+minutes = mdates.MinuteLocator(byminute=[0,5,10,15,20,25,30,35,40,45,50,55], interval=1)
+qMinutes = mdates.MinuteLocator(byminute=[0,15,30,45], interval=1)
+hours = mdates.HourLocator()
+xformatter = mdates.DateFormatter('%H:%M')
+
+with open('data/indoorlog.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        stime = re.search('\d\d:\d\d:\d\d', str(row[0])).group()
+        x.append(datetime.strptime(stime, '%H:%M:%S'))
+        rhum.append(float(row[1]))
+        avghum.append(float(row[2]))
+        if int(row[4]) == 1:
+            window.append(float(row[2]))
+        else:
+            window.append(None)
+
+print(window)
+#plt.plot(x, rhum, label='RHum',color='blue')
+plt.plot(x,avghum, label='avg. hum',color='blue')
+plt.plot(x,window, label='window', color='red')
+plt.xlabel('time')
+plt.ylabel('Humidity')
+plt.title('Indoor Humidity')
+plt.legend()
+plt.gcf().axes[0].xaxis.set_major_locator(qMinutes)
+plt.gcf().axes[0].xaxis.set_minor_locator(minutes)
+plt.gcf().axes[0].xaxis.set_major_formatter(xformatter)
+plt.gcf().autofmt_xdate()
+plt.show()
+
+
+#iTime, iHum, iAvg, iTemp, isOpen = np.loadtxt('datafiles/indoorlog.csv', delimiter=',', unpack=True)
