8f4dc50c09bc01f66b730e83a81adb8617fd5f84
SebastianLJ <sebastian.lund.j@gmail.com>
2021-03-26 17:02:09 +0100

    added buffering=1 to writer

--- DataProcessor/data/new_data/serialreader.py
+++ DataProcessor/data/new_data/serialreader.py
@@ -2,7 +2,7 @@ import serial
 from datetime import datetime
 import csv
 
-ser = serial.Serial('/dev/ttyACM1')
+ser = serial.Serial('COM4')
 ser.flushInput()
 now = datetime.now()
 part1 = datetime.now().strftime("%Y-%m-%d")
@@ -13,7 +13,7 @@ while True:
         ser_bytes = ser.readline()
         decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         print(decoded_bytes)
-        with open(part1 + "_" + part2 + ".csv","a") as f:
+        with open(part1 + "_" + part2 + ".csv","a", buffering=1) as f:
             writer = csv.writer(f,delimiter=",")
             writer.writerow([datetime.now().strftime("%H:%M:%S"),decoded_bytes])
     except:
